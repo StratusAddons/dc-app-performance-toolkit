284587686e24f90449f696a2a323379d0ab689e9
Aljosa Klisanic <aljosa@stratus-addons.com>
2023-12-19 09:49:01 +0100

    PlantUML DC testing

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -13,6 +13,25 @@ def app_specific_action(webdriver, datasets):
     if datasets['custom_pages']:
         app_specific_page_id = datasets['custom_page_id']
 
+    # To run action as specific user uncomment code bellow.
+    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
+    # just before test_2_selenium_z_log_out
+    # @print_timing("selenium_app_specific_user_login")
+    # def measure():
+    #     def app_specific_user_login(username='admin', password='admin'):
+    #         login_page = Login(webdriver)
+    #         login_page.delete_all_cookies()
+    #         login_page.go_to()
+    #         login_page.wait_for_page_loaded()
+    #         login_page.set_credentials(username=username, password=password)
+    #         login_page.click_login_button()
+    #         if login_page.is_first_login():
+    #             login_page.first_user_setup()
+    #         all_updates_page = AllUpdates(webdriver)
+    #         all_updates_page.wait_for_page_loaded()
+    #     app_specific_user_login(username='admin', password='admin')
+    # measure()
+
     @print_timing("selenium_app_custom_action")
     def measure():
 
@@ -20,6 +39,6 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR , "img.confluence-embedded-image.confluence-thumbnail.conf-macro"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, "[id^=stratus-addons-viewer]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
